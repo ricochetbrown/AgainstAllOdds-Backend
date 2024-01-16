256d0f14cb65d935282c31e5e8a7a8657623e9a7
ricochetbrown <ricochetbrown@gmail.com>
2024-01-16 15:56:42 -0500

    netlify

--- apps/myapp/src/main.rs
+++ apps/myapp/src/main.rs
@@ -1,4 +1,5 @@
-use actix_web::{App, HttpServer};
+use lambda_web::actix_web::{App, HttpServer};
+use lambda_web::{is_running_on_lambda, run_actix_on_lambda, LambdaError};
 
 use cats::{create_cat_data, create_cat_scope};
 
@@ -8,8 +9,13 @@ async fn main() -> std::io::Result<()> {
     // We need to make sure that the shared cat data is created once before the HttpServer
     // We can then pass this reference to the create_cat_scope so that all threads have access to the same data
     let cat_data = create_cat_data();
-    HttpServer::new(move || App::new().service(create_cat_scope(&cat_data)))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+
+    if is_running_on_lambda() {
+        // Run on AWS Lambda
+        run_actix_on_lambda(move || App::new().service(create_cat_scope(&cat_data))).await?;
+    } else {
+        // Run local server
+        HttpServer::new(move || App::new().service(create_cat_scope(&cat_data))).bind("127.0.0.1:8080")?.run().await?;
+    }
+    Ok(())
 }
