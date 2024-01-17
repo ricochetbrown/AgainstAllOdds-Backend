f2062d46f8d693caa291b4a2ac77141ca4e024f3
ricochetbrown <ricochetbrown@gmail.com>
2024-01-17 10:13:15 -0500

    netlify

---
+++ netlify/functions/kittens/src/main.rs
@@ -0,0 +1,24 @@
+use aws_lambda_events::event::apigw::{ApiGatewayProxyRequest, ApiGatewayProxyResponse};
+use aws_lambda_events::encodings::Body;
+use http::header::HeaderMap;
+use lambda_runtime::{handler_fn, Context, Error};
+use log::LevelFilter;
+use simple_logger::SimpleLogger;
+use actix_web::{App, HttpServer};
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    SimpleLogger::new().with_level(LevelFilter::Info).init().unwrap();
+
+    let func = handler_fn(my_handler);
+    lambda_runtime::run(func).await?;
+    Ok(())
+}
+
+pub(crate) async fn my_handler(event: ApiGatewayProxyRequest, _ctx: Context) -> Result<ApiGatewayProxyResponse, Error> {
+    let path = event.path.unwrap();
+
+    let resp = HttpServer::new(move || App::new().service(create_cat_scope(&cat_data))).bind(path)?.run().await?;
+
+    Ok(resp)
+}
