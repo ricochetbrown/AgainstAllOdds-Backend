b44ae3df65074a2696b314bc08f4b3e67c556294
ricochetbrown <ricochetbrown@gmail.com>
2024-01-18 19:05:39 -0500

    netlify

--- netlify/functions/kittens/src/main.rs
+++ netlify/functions/kittens/src/main.rs
@@ -1,8 +1,8 @@
+use aws_lambda_events::encodings::Body;
+use aws_lambda_events::event::apigw::{ApiGatewayProxyRequest, ApiGatewayProxyResponse};
 use http::header::CONTENT_TYPE;
-use lambda_http::{
-    run, service_fn, Body, Error, Request, RequestExt,
-    Response,
-};
+use http::HeaderMap;
+use lambda_runtime::{Context, Error, handler_fn};
 use serde::Serialize;
 use tracing::{debug, info, instrument};
 
@@ -10,30 +10,38 @@ use tracing::{debug, info, instrument};
 struct ApiResponse {
     data: String,
 }
-#[instrument]
-async fn function_handler(
-    event: Request,
-) -> Result<Response<Body>, Error> {
-    let who = event
-        .query_string_parameters_ref()
-        .and_then(|params| params.first("name"))
-        .unwrap_or("world");
-    info!(who, "query accepted");
-    let message = format!(
-        "Hello {who}, this is an Netlify serverless request"
-    );
-    let api_response = ApiResponse { data: message };
-    let body_text = serde_json::to_string(&api_response)?;
-    let resp = Response::builder()
-        .status(200)
-        .header(CONTENT_TYPE, "application/json")
-        .body(Body::Text(body_text))?;
-    Ok(resp)
-}
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     tracing_subscriber::fmt::init();
     debug!("cold boot");
-    run(service_fn(function_handler)).await
+
+    let func = handler_fn(my_handler);
+    lambda_runtime::run(func).await?;
+    Ok(())
+}
+
+#[instrument]
+pub(crate) async fn my_handler(event: ApiGatewayProxyRequest, _ctx: Context) -> Result<ApiGatewayProxyResponse, Error> {
+    let who = event
+        .query_string_parameters_ref()
+        .and_then(|params| params.first(&mut "name"))
+        .unwrap_or("world");
+    info!(who, "query accepted");
+
+    let message = format!(
+        "Hello {who}, this is an Netlify serverless request"
+    );
+    let api_response = ApiResponse { data: message };
+    let body_text = serde_json::to_string(&api_response)?;
+
+    let resp = ApiGatewayProxyResponse {
+        status_code: 200,
+        headers: HeaderMap::new(),
+        multi_value_headers: HeaderMap::new(),
+        body: Some(Body::Text(body_text)),
+        is_base64_encoded: Some(false),
+    };
+
+    Ok(resp)
 }
